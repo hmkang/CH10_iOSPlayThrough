9540623bd93ef660972d6dd7a0c9f1d0291aea7c
Hyunmin Kang <hmkang@hmkang.com>
2012-05-25 12:38:24 +0900

    Initial Commit

---
+++ CH10_iOSPlayThrough/CH10_iOSPlayThroughAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CH10_iOSPlayThroughAppDelegate.h
+//  CH10_iOSPlayThrough
+//
+//  Created by hmkang on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CH10_iOSPlayThroughAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ CH10_iOSPlayThrough/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CH10_iOSPlayThrough
+//
+//  Created by hmkang on 5/25/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CH10_iOSPlayThroughAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CH10_iOSPlayThroughAppDelegate class]));
+    }
+}
